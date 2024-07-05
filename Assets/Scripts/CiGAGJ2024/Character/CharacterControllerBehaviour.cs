40c59ad2e462bdd2ef82194954976ed68e5ec61d
Exonerator Adam <backroomscn.theexonerators.adam@outlook.com>
2024-07-05 13:01:10 +0800

    Basic Movement Script

--- Assets/Scripts/CiGAGJ2024/Character/CharacterControllerBehaviour.cs
+++ Assets/Scripts/CiGAGJ2024/Character/CharacterControllerBehaviour.cs
@@ -1,16 +1,93 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace CiGAGJ2024.Character
 {
-    [RequireComponent(typeof(CharacterControllerInputBehaviour))]
+    [
+        RequireComponent(typeof(CharacterControllerInputBehaviour)),
+        RequireComponent(typeof(Rigidbody2D)),
+        RequireComponent(typeof(Collider2D))
+    ]
     public class CharacterControllerBehaviour : MonoBehaviour
     {
+        [Header("References")]
         [SerializeField] protected CharacterControllerInputBehaviour InputBehaviour;
+        [SerializeField] protected Rigidbody2D Rigidbody;
+        [SerializeField] protected Collider2D Collider;
+        
+        [Header("Attributes")]
+        [SerializeField] private float maxSpeed = 6f;
+        [SerializeField] private float acceleration = 30f;
+        [SerializeField] private float deceleration = 60f;
+        [SerializeField] private float airControlFactor = 0.5f;
+        
+        public bool IsGrounded { get; set; }
+        
+        private Vector2 _velocity;
+        private float _movementValue = 0f, _movementTargetValue = 0f;
 
         protected void Awake()
         {
-            InputBehaviour = GetComponent<CharacterControllerInputBehaviour>();
+            if (!InputBehaviour) InputBehaviour = GetComponent<CharacterControllerInputBehaviour>();
+            if (!Rigidbody) Rigidbody = GetComponent<Rigidbody2D>();
+            if (!Collider) Collider = GetComponent<Collider2D>();
         }
+        
+        #region Core Functions
+        protected virtual void MovementCore()
+        {
+            float targetSpeed = _movementTargetValue * maxSpeed;
+            float speedDiff = targetSpeed - _velocity.x;
+            float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+
+            // Apply air control factor when not grounded
+            if (!IsGrounded)
+            {
+                accelRate *= airControlFactor;
+            }
+
+            // Calculate movement
+            float movement = speedDiff * accelRate * Time.deltaTime;
+
+            // Apply movement
+            _velocity.x += movement;
+
+            // Apply gravity
+            if (!IsGrounded)
+            {
+                _velocity.y += Physics2D.gravity.y * Time.deltaTime;
+            }
+
+            // Clamp velocity
+            _velocity.x = Mathf.Clamp(_velocity.x, -maxSpeed, maxSpeed);
+
+            // Apply velocity to Rigidbody
+            Rigidbody.velocity = _velocity;
+        }
+
+        protected virtual void MovementTickCore()
+        {
+            _movementValue = Mathf.Lerp(_movementValue, _movementTargetValue, Time.fixedDeltaTime);
+            _velocity = Rigidbody.velocity;
+        }
+        #endregion
+        
+        #region Input Listeners
+        public void ListenMovementPerformed(InputAction.CallbackContext context)
+        {
+            _movementTargetValue = context.ReadValue<float>();
+        }
+
+        public void ListenMovementCancelled(InputAction.CallbackContext context)
+        {
+            _movementTargetValue = 0f;
+        }
+
+        public void ListenJump(InputAction.CallbackContext context)
+        {
+            context.ReadValue<bool>();
+        }
+        #endregion
     }
 }
--- Assets/Scripts/CiGAGJ2024/Character/CharacterControllerInputBehaviour.cs
+++ Assets/Scripts/CiGAGJ2024/Character/CharacterControllerInputBehaviour.cs
@@ -1,9 +1,35 @@
+using System;
 using UnityEngine;
 
 namespace CiGAGJ2024.Character
 {
     public class CharacterControllerInputBehaviour : MonoBehaviour
     {
-        
+        [SerializeField] protected CharacterControllerBehaviour Controller;
+        protected CharacterControls InputMap;
+
+        protected void Awake()
+        {
+            if(!Controller) Controller = GetComponent<CharacterControllerBehaviour>();
+            InputMap = new CharacterControls();
+        }
+
+        protected void OnEnable()
+        {
+            if (InputMap is null) InputMap = new CharacterControls();
+            
+            InputMap.Enable();
+            InputMap.Primary.Movement.performed += Controller.ListenMovementPerformed;
+            InputMap.Primary.Movement.canceled += Controller.ListenMovementCancelled;
+            InputMap.Primary.Jump.performed += Controller.ListenJump;
+        }
+
+        protected void OnDisable()
+        {
+            InputMap.Disable();
+            InputMap.Primary.Movement.performed -= Controller.ListenMovementPerformed;
+            InputMap.Primary.Movement.canceled -= Controller.ListenMovementCancelled;
+            InputMap.Primary.Jump.performed -= Controller.ListenJump;
+        }
     }
 }
